95d66b7c45ff93eca7ac50830325427768f04291
bisratlike <bisratlike@gmail.com>
2025-03-03 21:01:45 +0300

    database setup

---
+++ taskmanager/drizzle/schema.ts
@@ -0,0 +1,11 @@
+import {pgTable , serial , text ,timestamp ,pgEnum} from "drizzle-orm/pg-core";
+export const taskStatus = pgEnum("task_status",["pending","completed","in_progress"]);
+export const Tasks = pgTable("users",{
+    id:serial("id").primaryKey(),
+    title:text("title"),
+    description:text("description"),
+    status:taskStatus("status").default("pending"),
+    dueDate:timestamp("due_date"),
+    createdAt:timestamp("created_at").defaultNow(),
+    updatedAt:timestamp("updated_at").defaultNow()
+})
